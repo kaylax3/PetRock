b93900899e1f51db56bc797eeb4adb05d6d7e31c
kaylax3 <hoytekayla@gmail.com>
2019-10-25 15:14:46 -0400

    Added Project
    10/25/19
    Added pet object to repository

---
+++ Assets/Script/HUDController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour
+{
+    public Text HungerText;
+    public Text FriendshipText;
+    public Text IsSickText;
+
+    public GameObject Rock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* HungerText.text = "I'm hungry";
+        FriendshipText.text = "Hello";
+        IsSickText.text = "Healthy"; */
+    }
+
+    // Update is called once per frame                                                     
+    void Update()
+    {
+        if (Rock != null)
+        {
+            HungerText.text = "Hunger: " + Rock.GetComponent<RockController>().hunger.ToString();
+            FriendshipText.text = "Friendship: " + Rock.GetComponent<RockController>().friendship.ToString();
+            IsSickText.text = "Is sick: " + Rock.GetComponent<RockController>().isSick.ToString();
+        }
+    }
+
+    public void FeedRock()
+    {
+        Rock.GetComponent<RockController>().hunger += 2;
+    }
+
+    public void PlayWithRock()
+    {
+        Rock.GetComponent<RockController>().friendship += 2;
+    }
+
+    public void GoToVet()
+    {
+        Rock.GetComponent<RockController>().isSick = false;
+    }
+}
